6fdeb3b7ff5ceddf0b2d51e997eeab800e7182e2
fedeoo <fedeoo.zf@gmail.com>
2019-10-28 21:17:07 +1100

    revert entry change

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -21,7 +21,7 @@ class AnimationCard extends StatelessWidget {
       animation: animation,
       builder: (BuildContext context, Widget child) {
         var transform = Matrix4.identity();
-        transform.setEntry(3, 2, 0);
+        transform.setEntry(3, 2, 0.001);
         if (direction == FlipDirection.VERTICAL) {
           transform.rotateX(animation.value);
         } else {
