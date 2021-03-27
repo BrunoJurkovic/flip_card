c4a668a98b30fc7bae26e4d46caffe4c8e915061
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-03-27 12:19:57 +0100

    Null safety migrated

--- lib/flip_card.dart
+++ lib/flip_card.dart
@@ -12,21 +12,21 @@ enum FlipDirection {
 class AnimationCard extends StatelessWidget {
   AnimationCard({this.child, this.animation, this.direction});
 
-  final Widget child;
-  final Animation<double> animation;
-  final FlipDirection direction;
+  final Widget? child;
+  final Animation<double>? animation;
+  final FlipDirection? direction;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: animation,
-      builder: (BuildContext context, Widget child) {
+      animation: animation!,
+      builder: (BuildContext context, Widget? child) {
         var transform = Matrix4.identity();
         transform.setEntry(3, 2, 0.001);
         if (direction == FlipDirection.VERTICAL) {
-          transform.rotateX(animation.value);
+          transform.rotateX(animation!.value);
         } else {
-          transform.rotateY(animation.value);
+          transform.rotateY(animation!.value);
         }
         return Transform(
           transform: transform,
@@ -48,9 +48,9 @@ class FlipCard extends StatefulWidget {
   /// The amount of milliseconds a turn animation will take.
   final int speed;
   final FlipDirection direction;
-  final VoidCallback onFlip;
-  final BoolCallback onFlipDone;
-  final FlipCardController controller;
+  final VoidCallback? onFlip;
+  final BoolCallback? onFlipDone;
+  final FlipCardController? controller;
 
   /// When enabled, the card will flip automatically when touched. This behavior
   /// can be disabled if this is not desired. To manually flip a card from your
@@ -80,9 +80,9 @@ class FlipCard extends StatefulWidget {
   final Alignment alignment;
 
   const FlipCard({
-    Key key,
-    @required this.front,
-    @required this.back,
+    Key? key,
+    required this.front,
+    required this.back,
     this.speed = 500,
     this.onFlip,
     this.onFlipDone,
@@ -100,9 +100,9 @@ class FlipCard extends StatefulWidget {
 
 class FlipCardState extends State<FlipCard>
     with SingleTickerProviderStateMixin {
-  AnimationController controller;
-  Animation<double> _frontRotation;
-  Animation<double> _backRotation;
+  AnimationController? controller;
+  Animation<double>? _frontRotation;
+  Animation<double>? _backRotation;
 
   bool isFront = true;
 
@@ -123,7 +123,7 @@ class FlipCardState extends State<FlipCard>
           weight: 50.0,
         ),
       ],
-    ).animate(controller);
+    ).animate(controller!);
     _backRotation = TweenSequence(
       <TweenSequenceItem<double>>[
         TweenSequenceItem<double>(
@@ -136,27 +136,27 @@ class FlipCardState extends State<FlipCard>
           weight: 50.0,
         ),
       ],
-    ).animate(controller);
-    controller.addStatusListener((status) {
+    ).animate(controller!);
+    controller!.addStatusListener((status) {
       if (status == AnimationStatus.completed ||
           status == AnimationStatus.dismissed) {
-        if (widget.onFlipDone != null) widget.onFlipDone(isFront);
+        if (widget.onFlipDone != null) widget.onFlipDone!(isFront);
       }
     });
 
-    widget?.controller?.state = this;
+    widget.controller?.state = this;
   }
 
   void toggleCard() {
     if (widget.onFlip != null) {
-      widget.onFlip();
+      widget.onFlip!();
     }
 
-    controller.duration = Duration(milliseconds: widget.speed);
+    controller!.duration = Duration(milliseconds: widget.speed);
     if (isFront) {
-      controller.forward();
+      controller!.forward();
     } else {
-      controller.reverse();
+      controller!.reverse();
     }
 
     setState(() {
@@ -186,7 +186,7 @@ class FlipCardState extends State<FlipCard>
     return child;
   }
 
-  Widget _buildContent({@required bool front}) {
+  Widget _buildContent({required bool front}) {
     // pointer events that would reach the backside of the card should be
     // ignored
     return IgnorePointer(
@@ -203,7 +203,7 @@ class FlipCardState extends State<FlipCard>
 
   @override
   void dispose() {
-    controller.dispose();
+    controller!.dispose();
     super.dispose();
   }
 }
