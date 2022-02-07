b8a8dc844a45519b36db38809ce5132657cee526
ciriousjoker <ciriousjoker@gmail.com>
2022-02-07 10:45:42 +0100

    skew() & hint() are now properly async

--- lib/flip_card_controller.dart
+++ lib/flip_card_controller.dart
@@ -22,32 +22,41 @@ class FlipCardController {
   /// Skew by amount percentage (0 - 1.0)
   /// This can be used with a MouseReagion to indicate that the card can
   /// be flipped. skew(0) to go back to original.
-  void skew(double amount, {Duration? duration, Curve? curve}) {
+  Future<void> skew(double amount, {Duration? duration, Curve? curve}) async {
     assert(0 <= amount && amount <= 1);
 
-    if (state!.isFront) {
-      controller?.animateTo(amount, duration: duration, curve: curve!);
-    } else {
-      controller?.animateTo(1 - amount, duration: duration, curve: curve!);
-    }
+    final target = state!.isFront ? amount : 1 - amount;
+    await controller
+        ?.animateTo(target, duration: duration, curve: curve ?? Curves.linear)
+        .asStream()
+        .first;
   }
 
   /// Triggers a flip animation that reverses after the duration
   /// and will run for `total`
-  void hint({Duration? duration, Duration? total}) {
+  Future<void> hint({Duration? duration, Duration? total}) async {
     assert(controller is AnimationController);
-    if (!(controller is AnimationController)) {
-      return;
-    }
+    if (!(controller is AnimationController)) return;
 
     if (controller!.isAnimating || controller!.value != 0) return;
 
+    final durationTotal = total ?? controller!.duration;
+
+    final completer = Completer();
+
     Duration? original = controller!.duration;
-    controller!.duration = total ?? controller!.duration;
+    controller!.duration = durationTotal;
     controller!.forward();
-    Timer(duration ?? const Duration(milliseconds: 150), () {
-      controller!.reverse();
+
+    final durationFlipBack = duration ?? const Duration(milliseconds: 150);
+
+    Timer(durationFlipBack, () {
+      controller!.reverse().whenComplete(() {
+        completer.complete();
+      });
       controller!.duration = original;
     });
+
+    await completer.future;
   }
 }
